990980f4af713b963982c350cc69ba3d912039d6
Sam Pibworth <sam.pibworth@lumilinks.com>
2022-04-08 10:46:42 +0100

    refactor and add transformed schema

--- Bink/models/marts/user.sql
+++ Bink/models/marts/user.sql
@@ -1,10 +1,34 @@
-WITH final AS (
-    SELECT
-        *
-    FROM
-        {{ ref('stg_user') }}
+WITH
+hist_users as (
+    SELECT *
+    FROM {{ ref('transformed_histuser')}}
 )
+
+,users as (
+    SELECT *
+    FROM {{ ref('stg_user')}}
+)
+
+,joined_user_records as (
+	SELECT
+		*
+	FROM
+		hist_users
+	WHERE 
+        ID not in
+		(
+			SELECT ID
+			FROM users
+		)
+	UNION ALL
+	SELECT
+		*
+	FROM
+		users
+)
+
+
 SELECT
     *
 FROM
-    final
+    joined_user_records
--- Bink/models/staging/stg_histuser.sql
+++ Bink/models/staging/stg_histuser.sql
@@ -1,39 +1,11 @@
-WITH
-current_users as (
+with hist_users as (
 	SELECT
 		*
 	FROM
-		{{ source('Bink', 'USER') }}
+		{{ source('Bink', 'HISTORY_USER') }}
 )
 
-,current_users_renamed as (
-	SELECT
-		APPLE				:: varchar	 AS APPLE,
-		CLIENT_ID			:: varchar	 AS CLIENT_ID,
-		DATE_JOINED			:: datetime	 AS DATE_JOINED,
-		DELETE_TOKEN		:: varchar	 AS DELETE_TOKEN,
-		EMAIL				:: varchar	 AS EMAIL,
-		EXTERNAL_ID			:: varchar	 AS EXTERNAL_ID,
-		FACEBOOK			:: varchar	 AS FACEBOOK,
-		ID					:: varchar	 AS ID,
-		IS_ACTIVE			:: boolean	 AS IS_ACTIVE,
-		IS_STAFF			:: boolean	 AS IS_STAFF,
-		IS_SUPERUSER		:: boolean	 AS IS_SUPERUSER,
-		IS_TESTER			:: boolean	 AS IS_TESTER,
-		LAST_LOGIN			:: datetime	 AS LAST_LOGIN,
-		MARKETING_CODE_ID	:: varchar	 AS MARKETING_CODE_ID,
-		PASSWORD			:: varchar	 AS PASSWORD,
-		RESET_TOKEN			:: varchar	 AS RESET_TOKEN,
-		SALT				:: varchar	 AS SALT,
-		TWITTER				:: varchar	 AS TWITTER,
-		UID					:: varchar	 AS UID,
-		MAGIC_LINK_VERIFIED	:: varchar	 AS MAGIC_LINK_VERIFIED,
-		FALSE				:: boolean	 AS IS_DELETED
-	FROM
-		current_users
-)
-
-select * from current_users_renamed
+select * from hist_users
 
 -- ,deleted_ids as (
 -- 	SELECT
