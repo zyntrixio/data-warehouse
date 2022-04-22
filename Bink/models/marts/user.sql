5460b95e61598f68c3afb3819388386505227f9f
Sam Pibworth <sam.pibworth@lumilinks.com>
2022-04-22 10:49:24 +0100

    add in null val

--- Bink/models/marts/user.sql
+++ Bink/models/marts/user.sql
@@ -46,7 +46,34 @@ users AS (
 		users
 )
 
+,users_na_unions AS (
+	SELECT
+		'NOT_APPLICABLE' AS USER_ID
+		,NULL AS UID
+		,NULL AS EXTERNAL_ID				
+		,NULL AS CLIENT_ID			
+		,NULL AS DATE_JOINED
+		,NULL AS DELETE_TOKEN		
+		,NULL AS EMAIL					
+		,NULL AS IS_ACTIVE			
+		,NULL AS IS_STAFF			
+		,NULL AS IS_SUPERUSER		
+		,NULL AS IS_TESTER			
+		,NULL AS LAST_LOGIN			
+		,NULL AS PASSWORD			
+		,NULL AS RESET_TOKEN
+		,NULL AS MARKETING_CODE_ID			
+		,NULL AS SALT
+		,NULL AS APPLE
+		,NULL AS FACEBOOK						
+		,NULL AS TWITTER									
+		,NULL AS MAGIC_LINK_VERIFIED
+	UNION ALL
+	SELECT *
+	FROM users_select
+)
+
 SELECT
     *
 FROM
-    users_select
+    users_na_unions
