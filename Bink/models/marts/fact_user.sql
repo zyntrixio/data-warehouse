622a04b400a7a2fd9a6ed34b89e74de04e046eaa
Sam Pibworth <sam.pibworth@lumilinks.com>
2022-05-04 16:18:50 +0100

    add a few columns

--- Bink/models/marts/fact_user.sql
+++ Bink/models/marts/fact_user.sql
@@ -20,9 +20,21 @@ user_events AS (
 	WHERE EVENT_TYPE IN ('user.created', 'user.deleted')
 )
 
-,user_events_select AS (
+,user_events_unpack AS (
 	SELECT
 		JSON:internal_user_ref::varchar as USER_ID
+		,EVENT_TYPE
+		,EVENT_DATE_TIME
+		,JSON:origin::varchar as ORIGIN
+		,JSON:channel::varchar as CHANNEL
+		,JSON:external_user_ref::varchar as EXTERNAL_USER_REF
+		,JSON:email::varchar as EMAIL		
+	FROM user_events
+)
+
+,user_events_select AS (
+	SELECT
+		USER_ID
 		,CASE WHEN EVENT_TYPE = 'user.created'
 			THEN 'CREATED'
 			WHEN EVENT_TYPE = 'user.deleted'
@@ -30,11 +42,18 @@ user_events AS (
 			ELSE NULL
 			END AS EVENT_TYPE
 		,EVENT_DATE_TIME
-		,JSON:origin::varchar as ORIGIN
-		,JSON:channel::varchar as CHANNEL
-		,JSON:external_user_ref::varchar as EXTERNAL_USER_REF
-		,JSON:email::varchar as EMAIL		
-	FROM user_events
+		,CASE WHEN
+			(EVENT_DATE_TIME = MAX(EVENT_DATE_TIME) OVER (PARTITION BY USER_ID))
+			THEN TRUE
+			ELSE FALSE
+			END AS IS_MOST_RECENT
+		,ORIGIN
+		,CHANNEL
+		,EXTERNAL_USER_REF
+		,LOWER(EMAIL) AS EMAIL
+		,SPLIT_PART(EMAIL,'@',2) AS DOMAIN
+	FROM user_events_unpack
+
 )
 
 
