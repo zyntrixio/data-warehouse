ea1e58ae7c2fc38b300e47a9d1c25d5d63576b0b
Sam Pibworth <sam.pibworth@lumilinks.com>
2022-06-27 11:10:14 +0100

    move ip to secret

--- Prefect/run_elt.py
+++ Prefect/run_elt.py
@@ -1,19 +1,21 @@
-from prefect import Flow, task, Parameter
+from prefect import Flow, task
 from prefect.tasks.dbt import DbtShellTask
 from prefect.tasks.airbyte.airbyte import AirbyteConnectionTask
 from prefect.run_configs import DockerRun
 from prefect.storage import Docker
 from prefect.schedules import Schedule, clocks
+from prefect.client.secrets import Secret
 import os
 
 DBT_DIRECTORY = 'Bink'
 DBT_PROFILE = 'Bink'
 AIRBYTE_EVENTS_CONNECTION_ID='62d2288c-11b2-4a5c-bbc1-4f0db35a9a93'
 AIRBYTE_HERMES_CONNECTION_ID='aa27ccee-6641-4de6-982a-37daf0700c16'
+AIRBYTE_IP=Secret("Bink_airbyte_ip").get()
 
 def make_airbyte_task(name, connection_id):
     return AirbyteConnectionTask(
-            airbyte_server_host='51.132.44.255'
+            airbyte_server_host=AIRBYTE_IP
             ,connection_id=connection_id
             ,name=name
         )
