5a532a48dda6a376255f5d32287e5bd4e033f84f
Sam Pibworth <sam.pibworth@lumilinks.com>
2022-07-12 11:08:29 +0100

    add a fact user secure for external userid

--- Bink/models/marts/bink/fact_user.sql
+++ Bink/models/marts/bink/fact_user.sql
@@ -21,24 +21,10 @@ Parameters:
 WITH
 user_events AS (
 	SELECT *
-	FROM {{ ref('stg_hermes__EVENTS')}}
-	WHERE EVENT_TYPE LIKE 'user%'
+	FROM {{ ref('fact_user_secure')}}
 	{% if is_incremental() %}
-  	AND _AIRBYTE_NORMALIZED_AT >= (SELECT MAX(INSERTED_DATE_TIME) from {{ this }})
-	{% endif %}	
-)
-
-,user_events_unpack AS (
-	SELECT
-		EVENT_ID
-		,EVENT_TYPE
-		,EVENT_DATE_TIME
-		,JSON:internal_user_ref::varchar as USER_ID
-		,JSON:origin::varchar as ORIGIN
-		,JSON:channel::varchar as CHANNEL
-		,JSON:external_user_ref::varchar as EXTERNAL_USER_REF
-		,JSON:email::varchar as EMAIL		
-	FROM user_events
+  	WHERE INSERTED_DATE_TIME >= (SELECT MAX(INSERTED_DATE_TIME) from {{ this }})
+	{% endif %}
 )
 
 ,user_events_select AS (
@@ -46,27 +32,15 @@ user_events AS (
 		EVENT_ID
 		,EVENT_DATE_TIME
 		,USER_ID
-		,CASE WHEN EVENT_TYPE = 'user.created'
-			THEN 'CREATED'
-			WHEN EVENT_TYPE = 'user.deleted'
-			THEN 'DELETED'
-			ELSE NULL
-			END AS EVENT_TYPE
-		,CASE WHEN
-			(EVENT_DATE_TIME = MAX(EVENT_DATE_TIME) OVER (PARTITION BY USER_ID))
-			THEN TRUE
-			ELSE FALSE
-			END AS IS_MOST_RECENT
+		,EVENT_TYPE
+		,IS_MOST_RECENT
 		,ORIGIN
 		,CHANNEL
 		// ,EXTERNAL_USER_REF
 		// ,LOWER(EMAIL) AS EMAIL
-		,SPLIT_PART(EMAIL,'@',2) AS DOMAIN
-		,SYSDATE() AS INSERTED_DATE_TIME
-	FROM user_events_unpack
-
+		,INSERTED_DATE_TIME
+	FROM user_events
 )
-
 
 SELECT
 	*
