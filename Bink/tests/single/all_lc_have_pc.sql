364010864d05cb3b981a96c5c421944d8995a289
Sam Pibworth <sam.pibworth@lumilinks.com>
2022-07-19 13:52:33 +0100

    all lc have pc

---
+++ Bink/tests/single/all_lc_have_pc.sql
@@ -0,0 +1,31 @@
+/*
+ Test to ensure no duplicate transaction ids that are not net zero spends - i.e refunds
+ 
+ Created By:     SP
+ Created Date:   2022/07/19
+ */
+
+
+{{ config(tags = ['business']) }}
+
+WITH new_lc AS (
+    SELECT *
+    FROM
+        {{ref('fact_loyalty_card_join')}}
+    WHERE
+        EVENT_TYPE = 'SUCCESS'
+        AND CHANNEL LIKE '%barclays%'
+        AND TIMEDIFF(
+                    HOUR, EVENT_DATE_TIME, (
+                        SELECT MAX(EVENT_DATE_TIME)
+                        FROM {{ref('fact_loyalty_card_join')}}
+                        )
+                    ) < 24
+)
+  
+SELECT
+    USER_ID
+FROM
+    new_lc
+WHERE
+    USER_ID NOT IN (SELECT USER_ID FROM {{ref('fact_payment_account')}})
