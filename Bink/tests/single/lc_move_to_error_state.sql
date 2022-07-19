10997b3c5561d323f71ca04f3cd4b51e6b5c2ac7
Sam Pibworth <sam.pibworth@lumilinks.com>
2022-07-19 11:10:47 +0100

    move to error state test

---
+++ Bink/tests/single/lc_move_to_error_state.sql
@@ -0,0 +1,38 @@
+/*
+ Test to ensure all lc are not ending with error states
+ 
+ Created By:     SP
+ Created Date:   2022/07/1
+ */
+
+
+{{ config(tags = ['business']) }}
+
+with lc_errors as (
+    SELECT
+        LOYALTY_CARD_ID
+    FROM {{ref('fact_loyalty_card_status_change')}}
+    WHERE IS_MOST_RECENT = true
+        AND TO_STATUS_ID not in (0,1)
+        AND TIMEDIFF(
+                hour, EVENT_DATE_TIME, (
+                    ELECT max(EVENT_DATE_TIME)
+                    FROM {{ref('fact_loyalty_card_status_change')}}
+                    )
+                ) < 24
+  )
+  
+,previously_valid as (
+    SELECT
+        LOYALTY_CARD_ID
+    FROM
+        {{ref('fact_loyalty_card_status_change')}}
+    WHERE
+        LOYALTY_CARD_ID IN (SELECT LOYALTY_CARD_ID FROM LC_ERRORS)
+        AND TO_STATUS_ID IN (1)
+    GROUP BY
+        LOYALTY_CARD_ID
+
+)
+
+select * from previously_valid
