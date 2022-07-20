182fac94760668d2272c751f0422edf9e6d68df4
Sam Pibworth <sam.pibworth@lumilinks.com>
2022-07-20 13:50:14 +0100

    fix tests

---
+++ Bink/tests/single/user_all_users_in_dim.sql
@@ -0,0 +1,23 @@
+/*
+Test to ensure no create user events are followed by another create user event
+
+Created By:     SP
+Created Date:   2022/07/12
+*/
+
+WITH new_users AS (
+    SELECT *
+    FROM {{ref('fact_user')}}
+    WHERE EVENT_TYPE = 'CREATED'
+    AND IS_MOST_RECENT = true
+    AND TIMEDIFF(
+                        HOUR, EVENT_DATE_TIME, (
+                            SELECT MAX(EVENT_DATE_TIME)
+                            FROM {{ref('fact_user')}}
+                            )
+                        ) < 24
+)
+
+SELECT *
+FROM new_users
+WHERE user_id NOT IN (SELECT user_id from {{ref('dim_user')}})
