2250b26840fb76289264805fdb01720d9f1d2ed4
Sam Pibworth <sam.pibworth@lumilinks.com>
2022-08-02 11:10:50 +0100

    fix merchant table

--- Bink/models/marts/bink/dim_merchant.sql
+++ Bink/models/marts/bink/dim_merchant.sql
@@ -26,13 +26,23 @@ merchant_identifier AS (
 	SELECT * 
     FROM {{ref('stg_harmonia__loyalty_scheme')}}
 )
+
+,consolidate_multiple_locations as (
+    SELECT
+        m.MERCHANT_ID
+        ,LAST_VALUE(M.LOCATION) OVER (PARTITION BY m.MERCHANT_ID ORDER BY m.ID) AS LOCATION
+        ,LAST_VALUE(M.POSTCODE) OVER (PARTITION BY m.MERCHANT_ID ORDER BY m.ID) AS POSTCODE
+        ,LAST_VALUE(M.LOCATION_ID) OVER (PARTITION BY m.MERCHANT_ID ORDER BY m.ID) AS LOCATION_ID
+        ,m.LOYALTY_SCHEME_ID
+        ,m.PAYMENT_PROVIDER_ID
+  FROM merchant_identifier m
+)
+
 ,merchant_select AS (
 SELECT
     m.MERCHANT_ID
     ,m.LOCATION
     ,m.POSTCODE
-    ,m.CREATED_AT
-    ,m.UPDATED_AT
     ,m.LOCATION_ID
     ,m.LOYALTY_SCHEME_ID
 	,l.slug AS LOYALTY_SCHEME_SLUG
@@ -48,9 +58,8 @@ SELECT
 		THEN true 
 		ELSE false
 		END AS payment_provider_amex
-    ,m.MERCHANT_INTERNAL_ID
 FROM
-	merchant_identifier m
+	consolidate_multiple_locations m
 LEFT JOIN payment_provider p
 	ON m.PAYMENT_PROVIDER_ID = p.id
 LEFT JOIN loyalty_scheme l
@@ -59,14 +68,10 @@ GROUP BY
     m.MERCHANT_ID
     ,m.LOCATION
     ,m.POSTCODE
-    ,m.CREATED_AT
-    ,m.UPDATED_AT
     ,m.LOCATION_ID
     ,m.LOYALTY_SCHEME_ID
-    ,m.MERCHANT_INTERNAL_ID
 	,l.slug
 )
-
 
 SELECT *
 FROM merchant_select
