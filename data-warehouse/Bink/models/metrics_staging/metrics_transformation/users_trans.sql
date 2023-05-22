87d3c0cb57782f3dfbe13f85714212e189c0d8ea
Chris Mitchell <cmitchell@bink.com>
2023-05-22 14:17:11 +0100

    user transf attempt 1

--- data-warehouse/Bink/models/metrics_staging/metrics_transformation/users_trans.sql
+++ data-warehouse/Bink/models/metrics_staging/metrics_transformation/users_trans.sql
@@ -5,44 +5,43 @@ Last modified by:
 Last modified date: 
 
 Description:
-    Channel table, which relates to the user_client tables in from user_facts, and to channel in events
+    User table, which relates to the transform date into do date and from date for metrics layer
 
 Parameters:
-    ref_object      - fact_user
+    ref_object      - src__fact_user
 					- dim_user?
 */
 
-WITH source AS (SELECT
-    EVENT_ID,
-    EVENT_DATE_TIME,
-    USER_ID,
-    EVENT_TYPE,
-    IS_MOST_RECENT,
-    ORIGIN,
-    CHANNEL,
-    BRAND,
-    EXTERNAL_USER_REF,
-    INSERTED_DATE_TIME,
-    UPDATED_DATE_TIME
-FROM
-    PROD.BINK_SECURE.FACT_USER
-)
-
-,join_one AS (
-    SELECT
-        s.EVENT_ID,
-        s.EVENT_DATE_TIME,
-        s.USER_ID,
-        s.EVENT_TYPE,
-        s.IS_MOST_RECENT,
-        s.ORIGIN,
-        s.CHANNEL,
-        s.BRAND,
-        s.EXTERNAL_USER_REF,
-        s.INSERTED_DATE_TIME,
-        s.UPDATED_DATE_TIME
-    FROM
-        source s
-)
-
-select * from join_one limit 100;
+WITH usr_events AS (SELECT event_id
+                         , event_date_time
+                         , user_id
+                         , event_type
+                         , is_most_recent
+                         , origin
+                         , channel
+                         , brand
+                         , inserted_date_time
+                         , updated_date_time
+                    FROM {{ref(src__fact_user)}})
+   , to_from_date AS (SELECT user_id
+                           , event_id
+                           , event_type
+                           , origin
+                           , channel
+                           , brand
+                           , event_date_time
+                           , LAG(event_date_time) OVER (PARTITION BY user_id ORDER BY event_date_time)  AS from_date
+                           , LEAD(event_date_time) OVER (PARTITION BY user_id ORDER BY event_date_time) AS to_date
+                      FROM usr_events)
+   , usr_stage AS (SELECT event_id
+                        , event_date_time
+                        , user_id
+                        , event_type
+                        , origin
+                        , channel
+                        , brand
+                        , COALESCE(from_date, event_date_time) AS from_date
+                        , COALESCE(to_date, event_date_time)   AS to_date
+                   FROM to_from_date)
+SELECT *
+FROM usr_stage
