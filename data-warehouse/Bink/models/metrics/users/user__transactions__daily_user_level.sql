33f621952f63c16b41da29c11248dbc422bea65e
Chris Mitchell <cmitchell@bink.com>
2023-06-07 11:28:42 +0100

    Created metric U007 Act Usr Usr Lvl

---
+++ data-warehouse/Bink/models/metrics/users/user__transactions__daily_user_level.sql
@@ -0,0 +1,42 @@
+/*
+Created by:         Christopher Mitchell 
+Created date:       2023-06-07
+Last modified by:   
+Last modified date: 
+
+Description:
+    Rewrite of the LL table lc_joins_links_snapshot and lc_joins_links containing both snapshot and daily absolute data of all link and join journeys split by merchant.
+Notes:
+    This code can be made more efficient if the start is pushed to the trans__lbg_user code and that can be the source for the majority of the dashboards including user_loyalty_plan_snapshot and user_with_loyalty_cards
+Parameters:
+    source_object       - src__fact_lc_add
+                        - src__fact_lc_removed
+                        - src__dim_loyalty_card
+                        - src__dim_date
+*/
+
+WITH user_events AS (
+    SELECT *
+    FROM {{ref('src__fact_transaction')}}
+)
+
+,metrics AS (
+    SELECT
+        DATE(DATE)                                          AS DATE
+        ,CHANNEL
+        ,BRAND
+        ,LOYALTY_PLAN_COMPANY
+        ,SUM(SPEND_AMOUNT)                                  AS SPEND
+        ,COALESCE(NULLIF(EXTERNAL_USER_REF,''), USER_ID)    AS U007__ACTIVE_USERS__USER_LEVEL_DAILY__NULL
+        ,COUNT(TRANSACTION_ID)                              AS TRANSACTIONS
+    FROM
+        user_events
+    GROUP BY
+        COALESCE(NULLIF(EXTERNAL_USER_REF,''), USER_ID)
+        ,CHANNEL
+        ,BRAND
+        ,LOYALTY_PLAN_COMPANY
+        ,DATE(DATE)
+)
+
+select * from metrics
