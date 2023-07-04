9e471e0924e8a04c9a48d065b645b802907ab36c
Chris Mitchell <cmitchell@bink.com>
2023-07-04 16:36:28 +0100

    comments

--- data-warehouse/Bink/models/metrics/voucher/voucher__daily_channel_brand_retailer.sql
+++ data-warehouse/Bink/models/metrics/voucher/voucher__daily_channel_brand_retailer.sql
@@ -1,3 +1,15 @@
+/*
+Created by:         Christopher Mitchell 
+Created date:       2023-07-04
+Last modified by:   
+Last modified date: 
+
+Description:
+    todo
+Parameters:
+    source_object       - voucher_trans
+*/
+
 WITH voucher_trans AS (
     SELECT *
     FROM {{ ref('voucher_trans') }})
