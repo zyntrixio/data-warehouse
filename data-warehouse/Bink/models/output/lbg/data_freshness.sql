333e103d1abcb64420a049b5851de8ee821b8f70
Anand Bhakta <abhakta@bink.com>
2023-07-06 13:24:46 +0100

    addding data freshness

---
+++ data-warehouse/Bink/models/output/lbg/data_freshness.sql
@@ -0,0 +1,13 @@
+/*
+Created by:         Anand Bhakta
+Created date:       2023-06-07
+Last modified by:   
+Last modified date: 
+
+Description:
+    Datasource to show freshness of data in the metrics layer
+Parameters:
+    source_object       - src__fact_lc
+*/
+
+SELECT MAX(INSERTED_DATE_TIME) AS DATA_FRESHNESS from {{ref('src__fact_lc')}}
