ce83f31cd2547ba7cf428d9ab53f12b3f45c884c
Chris Mitchell <cmitchell@bink.com>
2023-07-06 12:09:47 +0100

    DP170 done

--- data-warehouse/Bink/models/output/commercial/revenue_share_lbg.sql
+++ data-warehouse/Bink/models/output/commercial/revenue_share_lbg.sql
@@ -12,24 +12,48 @@ Parameters:
 */
 
 WITH joins AS (
-    SELECT *
-        , 'JOINS' AS TAB
+    SELECT date
+         , channel
+         , brand
+         , loyalty_plan_name
+         , loyalty_plan_company
+         , lc053__successful_loyalty_card_joins__monthly_channel_brand_retailer__dcount_user
+         , 'JOINS' AS tab
     FROM {{ ref('lc__links_joins__monthly_retailer_channel') }}
-    WHERE CHANNEL = 'LLOYDS'
-    AND LOYALTY_PLAN_COMPANY NOT IN ('Loyalteas', 'Bink Sweet Shop')
-    )
+    WHERE channel = 'LLOYDS'
+      AND loyalty_plan_company NOT IN ('Loyalteas', 'Bink Sweet Shop'))
 
-    , active AS (
-        SELECT *
-            ,'ACTIVE_USERS' AS TAB
-        FROM {{ ref('user__transactions__monthly_user_level') }}
-        WHERE CHANNEL = 'LLOYDS'
-        AND LOYALTY_PLAN_COMPANY NOT IN ('Loyalteas', 'Bink Sweet Shop')
-    )
+   , active AS (
+    SELECT date
+         , channel
+         , brand
+         , loyalty_plan_company
+         , u107__active_users_brand_retailer_monthly__dcount_user
+         , 'ACTIVE' AS tab
+    FROM {{ ref('user__transactions__monthly_user_level') }}
+    WHERE channel = 'LLOYDS'
+      AND loyalty_plan_company NOT IN ('Loyalteas', 'Bink Sweet Shop'))
 
-    , combine AS (
-        SELECT *
-        FROM joins
-    )
+   , combine AS (
+    SELECT date
+         , tab
+         , channel
+         , brand
+         , loyalty_plan_name
+         , loyalty_plan_company
+         , lc053__successful_loyalty_card_joins__monthly_channel_brand_retailer__dcount_user
+         , NULL AS u107__active_users_brand_retailer_monthly__dcount_user
+    FROM joins
+    UNION ALL
+    SELECT date
+         , tab
+         , channel
+         , brand
+         , loyalty_plan_company
+         , NULL AS loyalty_plan_name
+         , NULL AS lc053__successful_loyalty_card_joins__monthly_channel_brand_retailer__dcount_user
+         , u107__active_users_brand_retailer_monthly__dcount_user
+    FROM active)
 
-SELECT * FROM combine
+SELECT *
+FROM combine
