0249b49c8417b1f8b3f3bfd9412aa60551e42047
Chris Mitchell <cmitchell@bink.com>
2023-08-10 16:43:09 +0100

    Updt all events and added dupe txns

---
+++ data-warehouse/Bink/tests/single/all_dupe_txns_not_export.sql
@@ -0,0 +1,21 @@
+/*
+ Test to ensure all duplicate transactions are not visable in the exported transactions
+ 
+ Created By:     Chris Mitchell
+ Created Date:   2023-08-09
+ */
+
+{{ config(
+        tags=['business']
+        ,error_if = '>10'
+        ,warn_if = '>1'
+        ,meta={"description": "Test to ensure all duplicate transactions are not visable in the exported transactions", 
+            "test_type": "Business"},
+) }}
+
+SELECT t1.transaction_id, t1.event_id
+FROM {{ ref('fact_transaction_secure') }} t1
+         JOIN {{ ref('fact_transaction_secure') }} t2 ON t1.transaction_id = t2.transaction_id
+WHERE t1.duplicate_transaction = 'True'
+  AND t1.transaction_id IS NOT NULL
+  AND t1.event_id <> t2.event_id
