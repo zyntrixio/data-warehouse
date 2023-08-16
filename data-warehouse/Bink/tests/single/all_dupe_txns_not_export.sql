c3ae10db6f1145d16cd507b653ec7abdb852d3f1
Chris Mitchell <cmitchell@bink.com>
2023-08-16 14:26:52 +0100

    changes to dedupe txns test

--- data-warehouse/Bink/tests/single/all_dupe_txns_not_export.sql
+++ data-warehouse/Bink/tests/single/all_dupe_txns_not_export.sql
@@ -3,20 +3,26 @@
  
  Created By:     Chris Mitchell
  Created Date:   2023-08-09
- force git update
+
  */
 
 {{ config(
         tags=['business']
-        ,error_if = '>10'
-        ,warn_if = '>1'
+        ,error_if = '>0'
         ,meta={"description": "Test to ensure all duplicate transactions are not visable in the exported transactions", 
             "test_type": "Business"},
 ) }}
 
-SELECT t1.transaction_id, t1.event_id
-FROM {{ ref('fact_transaction_secure') }} t1
-         JOIN {{ ref('fact_transaction_secure') }} t2 ON t1.transaction_id = t2.transaction_id
-WHERE t1.duplicate_transaction = 'True'
-  AND t1.transaction_id IS NOT NULL
-  AND t1.event_id <> t2.event_id
+WITH dupes AS (
+    SELECT * FROM {{ ref('fact_transaction_secure') }} WHERE duplicate_transaction = TRUE)
+
+   , exports AS (
+    SELECT * FROM {{ ref('fact_transaction_secure') }}  WHERE duplicate_transaction = FALSE)
+
+   , test_outputs AS (
+    SELECT d.transaction_id
+    FROM dupes d
+             INTERSECT SELECT e.transaction_id FROM exports e)
+
+SELECT *
+FROM test_outputs
