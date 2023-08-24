f05dd66aa70a17b3d681cd78a365907b9558395e
Chris Mitchell <cmitchell@bink.com>
2023-08-24 10:35:01 +0100

    Auto fmt

--- data-warehouse/Bink/models/prod/bink/fact_loyalty_card.sql
+++ data-warehouse/Bink/models/prod/bink/fact_loyalty_card.sql
@@ -5,7 +5,7 @@ Last modified by:   Christopher Mitchell
 Last modified date: 2023-06-05
 
 Description:
-	Fact LC 
+	Fact LC
 
 Parameters:
     ref_object      - fact_loyalty_card_secure
@@ -19,44 +19,46 @@ Parameters:
 }}
 
 with
-    lc as (
-        select *
-        from {{ ref("fact_loyalty_card_secure") }}
-        {% if is_incremental() %}
-        where updated_date_time >= (select max(updated_date_time) from {{ this }})
-        {% endif %}
-    ),
-    lc_select as (
-        select
-            event_id,
-            event_date_time,
-            auth_type,
-            event_type,
-            loyalty_card_id,
-            loyalty_plan,
-            loyalty_plan_name,
-            loyalty_plan_company,
-            is_most_recent,
-            case
-                when main_answer is null
+lc as (
+    select *
+    from {{ ref("fact_loyalty_card_secure") }}
+    {% if is_incremental() %}
+        where
+            updated_date_time >= (select max(updated_date_time) from {{ this }})
+    {% endif %}
+),
+
+lc_select as (
+    select
+        event_id,
+        event_date_time,
+        auth_type,
+        event_type,
+        loyalty_card_id,
+        loyalty_plan,
+        loyalty_plan_name,
+        loyalty_plan_company,
+        is_most_recent,
+        case
+            when main_answer is null
                 then null
-                when main_answer = ''
+            when main_answer = ''
                 then null
-                else md5(main_answer)
-            end as main_answer,
-            channel,
-            brand,
-            origin,
-            user_id,
-            -- external_user_ref,
-            -- email,
-            email_domain,
-            consent_slug,
-            consent_response,
-            inserted_date_time,
-            updated_date_time
-        from lc
-    )
+            else md5(main_answer)
+        end as main_answer,
+        channel,
+        brand,
+        origin,
+        user_id,
+        -- external_user_ref,
+        -- email,
+        email_domain,
+        consent_slug,
+        consent_response,
+        inserted_date_time,
+        updated_date_time
+    from lc
+)
 
 select *
 from lc_select
--- data-warehouse/Bink/models/prod/bink_secure/fact_transaction_secure.sql
+++ data-warehouse/Bink/models/prod/bink_secure/fact_transaction_secure.sql
@@ -17,90 +17,95 @@ Parameters:
 }}
 
 with
-    transaction_events as (
-        select *
-        from {{ ref("transformed_hermes_events") }}
-        where
-            event_type in ('transaction.exported', 'transaction.duplicate')
-            {% if is_incremental() %}
+transaction_events as (
+    select *
+    from {{ ref("transformed_hermes_events") }}
+    where
+        event_type in ('transaction.exported', 'transaction.duplicate')
+        {% if is_incremental() %}
             and _airbyte_normalized_at
             >= (select max(inserted_date_time) from {{ this }})
-            {% endif %}
-    ),
-    loyalty_plan as (select * from {{ ref("stg_hermes__SCHEME_SCHEME") }}),
-    dim_user as (select * from {{ ref("stg_hermes__USER") }}),
-    dim_channel as (select * from {{ ref("stg_hermes__CLIENT_APPLICATION") }}),
-    transaction_events_unpack as (
-        select
-            event_id,
-            event_type,
-            event_date_time,
-            json:internal_user_ref::varchar as user_id,
-            json:transaction_id::varchar as transaction_id,
-            json:provider_slug::varchar as provider_slug,
-            json:feed_type::varchar as feed_type,
-            json:transaction_date::datetime as transaction_date,
-            json:spend_amount / 100::number(12, 2) as spend_amount,
-            json:spend_currency::varchar as spend_currency,
-            json:loyalty_id::varchar as loyalty_id,
-            json:scheme_account_id::varchar as loyalty_card_id,
-            json:mid::varchar as merchant_id,
-            -- json:location_id::varchar as location_id NOTE: joins to harmonia merchant data,
-            -- json:merchant_internal_id::varchar as merchant_internal_id NOTE: joins to harmonia merchant data,
-            json:payment_card_account_id::varchar as payment_account_id,
-            json:settlement_key::varchar as settlement_key,
-            json:authorisation_code::varchar as auth_code,
-            json:approval_code::varchar as approval_code
+        {% endif %}
+),
 
-        from transaction_events
-    ),
-    select_transactions as (
-        select
-            event_id,
-            event_date_time,
-            t.user_id,
-            u.external_id as external_user_ref,
-            case
-                when c.channel_name in ('Bank of Scotland', 'Lloyds', 'Halifax')
+loyalty_plan as (select * from {{ ref("stg_hermes__SCHEME_SCHEME") }}),
+
+dim_user as (select * from {{ ref("stg_hermes__USER") }}),
+
+dim_channel as (select * from {{ ref("stg_hermes__CLIENT_APPLICATION") }}),
+
+transaction_events_unpack as (
+    select
+        event_id,
+        event_type,
+        event_date_time,
+        json:internal_user_ref::varchar as user_id,
+        json:transaction_id::varchar as transaction_id,
+        json:provider_slug::varchar as provider_slug,
+        json:feed_type::varchar as feed_type,
+        json:transaction_date::datetime as transaction_date,
+        json:spend_amount / 100::number(12, 2) as spend_amount,
+        json:spend_currency::varchar as spend_currency,
+        json:loyalty_id::varchar as loyalty_id,
+        json:scheme_account_id::varchar as loyalty_card_id,
+        json:mid::varchar as merchant_id,
+        -- json:location_id::varchar as location_id NOTE: joins to harmonia merchant data,
+        -- json:merchant_internal_id::varchar as merchant_internal_id NOTE: joins to harmonia merchant data,
+        json:payment_card_account_id::varchar as payment_account_id,
+        json:settlement_key::varchar as settlement_key,
+        json:authorisation_code::varchar as auth_code,
+        json:approval_code::varchar as approval_code
+
+    from transaction_events
+),
+
+select_transactions as (
+    select
+        event_id,
+        event_date_time,
+        t.user_id,
+        u.external_id as external_user_ref,
+        case
+            when c.channel_name in ('Bank of Scotland', 'Lloyds', 'Halifax')
                 then 'LLOYDS'
-                when c.channel_name = 'Barclays Mobile Banking'
+            when c.channel_name = 'Barclays Mobile Banking'
                 then 'BARCLAYS'
-                when c.channel_name = 'Bink'
+            when c.channel_name = 'Bink'
                 then 'BINK'
-                else null
-            end as channel,
-            case
-                when c.channel_name in ('Bink', 'Lloyds', 'Halifax')
+            else null
+        end as channel,
+        case
+            when c.channel_name in ('Bink', 'Lloyds', 'Halifax')
                 then upper(c.channel_name)
-                when c.channel_name = 'Barclays Mobile Banking'
+            when c.channel_name = 'Barclays Mobile Banking'
                 then 'BARCLAYS'
-                when c.channel_name = 'Bank of Scotland'
+            when c.channel_name = 'Bank of Scotland'
                 then 'BOS'
-                else null
-            end as brand,
-            transaction_id,
-            provider_slug,
-            feed_type,
-            event_type = 'transaction.duplicate' as duplicate_transaction,
-            lp.loyalty_plan_name,
-            lp.loyalty_plan_company,
-            transaction_date,
-            spend_amount,
-            spend_currency,
-            loyalty_id,
-            loyalty_card_id,
-            merchant_id,
-            payment_account_id,
-            settlement_key,
-            auth_code,
-            approval_code,
-            sysdate() as inserted_date_time,
-            sysdate() as updated_date_time
-        from transaction_events_unpack t
-        left join loyalty_plan lp on lp.loyalty_plan_slug = t.provider_slug
-        left join dim_user u on u.user_id = t.user_id
-        left join dim_channel c on u.channel_id = c.channel_id
-    )
+            else null
+        end as brand,
+        transaction_id,
+        provider_slug,
+        feed_type,
+        event_type = 'transaction.duplicate' as duplicate_transaction,
+        lp.loyalty_plan_name,
+        lp.loyalty_plan_company,
+        transaction_date,
+        spend_amount,
+        spend_currency,
+        loyalty_id,
+        loyalty_card_id,
+        merchant_id,
+        payment_account_id,
+        settlement_key,
+        auth_code,
+        approval_code,
+        sysdate() as inserted_date_time,
+        sysdate() as updated_date_time
+    from transaction_events_unpack t
+    left join loyalty_plan lp on lp.loyalty_plan_slug = t.provider_slug
+    left join dim_user u on u.user_id = t.user_id
+    left join dim_channel c on u.channel_id = c.channel_id
+)
 
 select *
 from select_transactions
