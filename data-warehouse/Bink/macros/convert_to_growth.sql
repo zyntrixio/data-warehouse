a8d94fde07dada3e3964d5bfe82a3c1bdbc6d3fb
Anand Bhakta <abhakta@bink.com>
2023-09-20 16:01:25 +0100

    functioning code with growth macro

---
+++ data-warehouse/Bink/macros/convert_to_growth.sql
@@ -0,0 +1,44 @@
+{# 
+Created by:         Anand Bhakta
+Created date:       2023-09-20
+Last modified by:   
+Last modified date: 
+
+Description:
+    This macro takes in a table or source table reference and then returns the columns as comparisons based on date 
+    dynamically. This can also seperate out metrics from categorical inputs.
+
+Parameters:
+    node     - takes in table reference
+    categorical       - takes list of string inputs which are columns from base table not to be converted to growth but included in select
+    exclusion       - takes list of string inputs referencing columns in base table to exclude from query
+    partition      - takes list of string inputs referencing columns in base table to exclude from query
+    order      - takes a string to order data by
+
+Returns:
+        Returns a select statement to generate model
+    
+    
+#}
+
+{% macro convert_to_growth(node, categorical, exclusion, partition, order) %}
+
+with metrics as (select * from {{ ref(node) }})
+
+,growth as (
+    select
+        {%- for col in adapter.get_columns_in_relation(ref(node)) -%}
+        {%- if col.column in categorical %}
+        {{ col.column}},
+        {%- else %}
+        div0({{ col.column}} - 1, lag({{ col.column}}) over (partition by {% for col in partition %} {{col}} {%- if not loop.last %} , {% endif -%} {% endfor %} order by {{order}})) as {{ col.column}}__growth
+        {%- if not loop.last %} , {% endif -%}
+        {%- endif %}
+        {%- endfor %}
+    from
+        metrics
+)
+
+select * from growth
+
+{% endmacro %}
