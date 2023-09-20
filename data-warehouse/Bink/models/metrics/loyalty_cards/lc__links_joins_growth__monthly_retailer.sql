e186b59c4247be239478d4a004727e311c174971
Anand Bhakta <abhakta@bink.com>
2023-09-20 15:25:16 +0100

    functioning growth query

--- data-warehouse/Bink/models/metrics/loyalty_cards/lc__links_joins_growth__monthly_retailer.sql
+++ data-warehouse/Bink/models/metrics/loyalty_cards/lc__links_joins_growth__monthly_retailer.sql
@@ -15,24 +15,25 @@ Parameters:
 {% endfor %}
 #}
 
-{% set categorical = ["date", "loyalty_plan_name", "loyalty_plan_company"] %}
+{% set categorical = ["DATE", "LOYALTY_PLAN_NAME", "LOYALTY_PLAN_COMPANY"] %}
 {% set exclusion = [] %}
-{% set partition = ["loyalty_plan_name", "loyalty_plan_company"] %}
-{% set order = ["date"] %}
+{% set partition = ["LOYALTY_PLAN_NAME", "LOYALTY_PLAN_COMPANY"] %}
+{% set order = "DATE" %}
 
 with metrics as (select * from {{ ref("lc__links_joins__monthly_retailer") }})
 
 ,lag as (
     select
-        {% for col in adapter.get_columns_in_relation(ref('lc__links_joins__monthly_retailer')) if col.column in categorical -%}
-
+        {%- for col in adapter.get_columns_in_relation(ref('lc__links_joins__monthly_retailer')) -%}
+        {%- if col.column in categorical %}
         {{ col.column}},
-
-        {% else %}
-
-        div0({{ col.column}} - 1, lag({{ col.column}}) over (partition by loyalty_plan_name, loyalty_plan_company order by date)),
-
-        {% endfor %}
+        {%- else %}
+        div0({{ col.column}} - 1, lag({{ col.column}}) over (partition by {% for col in partition %} {{col}} {%- if not loop.last %} , {% endif -%} {% endfor %} order by {{order}}))
+        {%- if not loop.last %} , {% endif -%}
+        {%- endif %}
+        {%- endfor %}
+    from
+        metrics
 )
 
 select * from lag
