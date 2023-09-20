f9a538d2e3cd487b8d731fba99b1ff8ce32f78fd
Anand Bhakta <abhakta@bink.com>
2023-09-20 16:00:41 +0100

    functioning code without macro

--- data-warehouse/Bink/models/metrics/loyalty_cards/lc__links_joins_growth__monthly_retailer.sql
+++ data-warehouse/Bink/models/metrics/loyalty_cards/lc__links_joins_growth__monthly_retailer.sql
@@ -15,16 +15,17 @@ Parameters:
 {% endfor %}
 #}
 
+{% set node = "lc__links_joins__monthly_retailer" %}
 {% set categorical = ["DATE", "LOYALTY_PLAN_NAME", "LOYALTY_PLAN_COMPANY"] %}
 {% set exclusion = [] %}
 {% set partition = ["LOYALTY_PLAN_NAME", "LOYALTY_PLAN_COMPANY"] %}
 {% set order = "DATE" %}
 
-with metrics as (select * from {{ ref("lc__links_joins__monthly_retailer") }})
+with metrics as (select * from {{ ref(node) }})
 
 ,lag as (
     select
-        {%- for col in adapter.get_columns_in_relation(ref('lc__links_joins__monthly_retailer')) -%}
+        {%- for col in adapter.get_columns_in_relation(ref(node)) -%}
         {%- if col.column in categorical %}
         {{ col.column}},
         {%- else %}
