daa9cff3ae493e639e889299167927da2e906ef8
Chris Mitchell <cmitchell@bink.com>
2023-09-27 11:37:09 +0100

    refactor jira_trans and output

--- data-warehouse/Bink/models/output/jira/data_product_squad.sql
+++ data-warehouse/Bink/models/output/jira/data_product_squad.sql
@@ -1,6 +1,25 @@
+/*
+Created by:         Christopher Mitchell
+Created date:       2023-09-27
+Last modified by:
+Last modified date:
+
+Description:
+    output layer for Jira Data Product BI
+Parameters:
+    source_object       - jira_trans
+*/
+
 with stage as (
     select *
-    from {{ ref('stg_metrics__jira_data_product') }}
+    from {{ ref('jira_trans') }}
+    where team = 'Data'
 )
 
-select * from stage
+select
+    team,
+    name,
+    story_points_in_sprint_goal,
+    story_points_carried_over,
+    defects_in_sprint
+from stage
--- data-warehouse/Bink/models/output/jira/wallet_squad.sql
+++ data-warehouse/Bink/models/output/jira/wallet_squad.sql
@@ -1,6 +1,25 @@
+/*
+Created by:         Christopher Mitchell
+Created date:       2023-09-27
+Last modified by:
+Last modified date:
+
+Description:
+    output layer for Jira Wallet BI
+Parameters:
+    source_object       - jira_trans
+*/
+
 with stage as (
     select *
-    from {{ ref('stg_metrics__jira_wallet') }}
+    from {{ ref('jira_trans') }}
+    where team = 'Wallet'
 )
 
-select * from stage
+select
+    team,
+    name,
+    story_points_in_sprint_goal,
+    story_points_carried_over,
+    defects_in_sprint
+from stage
