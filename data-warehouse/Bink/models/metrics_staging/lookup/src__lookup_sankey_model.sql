70b4ce131bbe0d7afe2c16a7d23234faaeaa68d3
Chris Mitchell <cmitchell@bink.com>
2023-10-10 15:02:28 +0100

    sources

--- data-warehouse/Bink/models/metrics_staging/lookup/src__lookup_sankey_model.sql
+++ data-warehouse/Bink/models/metrics_staging/lookup/src__lookup_sankey_model.sql
@@ -1,3 +1,14 @@
+/*
+Created by:         Anand Bhakta
+Created date:       2023-02-05
+Last modified by:   Christopher Mitchell
+Last modified date: 2023-08-24
+
+Description:
+    Source file for LBG Sankey Funnel Mode
+Parameters:
+    source_object       - source("RAW_BINK_LOOKUP", "SANKEY_MODEL")
+*/
 with source as (select * from {{ source("RAW_BINK_LOOKUP", "SANKEY_MODEL") }})
 
 select *
--- data-warehouse/Bink/models/metrics_staging/lookup/src__lookup_status_mapping.sql
+++ data-warehouse/Bink/models/metrics_staging/lookup/src__lookup_status_mapping.sql
@@ -1,3 +1,14 @@
+/*
+Created by:         Anand Bhakta
+Created date:       2023-02-05
+Last modified by:   Christopher Mitchell
+Last modified date: 2023-08-24
+
+Description:
+    Source file for status mapping for scheme account status
+Parameters:
+    source_object       - source("STAGING", "STG_LOOKUP__SCHEME_ACCOUNT_STATUS")
+*/
 with
 source as (
     select * from {{ source("STAGING", "STG_LOOKUP__SCHEME_ACCOUNT_STATUS") }}
