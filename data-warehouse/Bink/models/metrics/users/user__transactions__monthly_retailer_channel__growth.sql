8b49c78a890f71251216cae97cbbfed187c4e7c9
Anand Bhakta <abhakta@bink.com>
2023-11-09 17:14:44 +0000

    adding growth metrics to retailer channel metrics

---
+++ data-warehouse/Bink/models/metrics/users/user__transactions__monthly_retailer_channel__growth.sql
@@ -0,0 +1,18 @@
+/*
+Created by:         Anand Bhakta
+Created date:       2023-09-25
+Last modified by:
+Last modified date:
+
+Description:
+    Convert user__transactions__monthly_retailer to growth
+Parameters:
+    source_object       - user__transactions__monthly_retailer
+*/
+
+{{convert_to_growth("user__transactions__monthly_retailer_channel",
+                    ["DATE", "LOYALTY_PLAN_NAME", "LOYALTY_PLAN_COMPANY", "CHANNEL", "BRAND"],
+                    [],
+                    ["LOYALTY_PLAN_NAME", "LOYALTY_PLAN_COMPANY", "CHANNEL", "BRAND"],
+                    "DATE" )
+}}
