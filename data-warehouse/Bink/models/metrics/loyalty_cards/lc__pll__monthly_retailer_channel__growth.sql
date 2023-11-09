ad1611e12156f68155219783be124966bf6d07f9
Anand Bhakta <abhakta@bink.com>
2023-11-09 17:14:44 +0000

    adding growth metrics to retailer channel metrics

---
+++ data-warehouse/Bink/models/metrics/loyalty_cards/lc__pll__monthly_retailer_channel__growth.sql
@@ -0,0 +1,18 @@
+/*
+Created by:         Anand Bhakta
+Created date:       2023-11-09
+Last modified by:
+Last modified date:
+
+Description:
+    Convert the lc__pll__monthly_retailer table to growth metrics.
+Parameters:
+    source_object       - lc__pll__monthly_retailer
+*/
+
+{{convert_to_growth("lc__pll__monthly_retailer_channel",
+                    ["DATE", "LOYALTY_PLAN_NAME", "LOYALTY_PLAN_COMPANY", "CHANNEL", "BRAND"],
+                    [],
+                    ["LOYALTY_PLAN_NAME", "LOYALTY_PLAN_COMPANY", "CHANNEL", "BRAND"],
+                    "DATE" )
+}}
---
+++ data-warehouse/Bink/models/metrics/transactions/trans__avg__monthly_retailer_channel__growth.sql
@@ -0,0 +1,18 @@
+/*
+Created by:         Anand Bhakta
+Created date:       2023-09-25
+Last modified by:
+Last modified date:
+
+Description:
+    Convert the trans__avg__monthly_retailer table to growth metrics.
+Parameters:
+    source_object       - trans__avg__monthly_retailer_channe;
+*/
+
+{{convert_to_growth("trans__avg__monthly_retailer_channel",
+                    ["DATE", "LOYALTY_PLAN_NAME", "LOYALTY_PLAN_COMPANY", "CHANNEL", "BRAND"],
+                    [],
+                    ["LOYALTY_PLAN_NAME", "LOYALTY_PLAN_COMPANY", "CHANNEL", "BRAND"],
+                    "DATE" )
+}}
