4839952f75b0114f373aa796df31fd82d4abb2ae
Chris Mitchell <cmitchell@bink.com>
2023-12-13 11:31:26 +0000

    users and rename models

---
+++ data-warehouse/Bink/models/output/ext/ext_dashboard_view_usr.sql
@@ -0,0 +1,23 @@
+/*
+CREATED BY:         CHRISTOPHER MITCHELL
+CREATED DATE:       2023-11-12
+LAST MODIFIED BY:   
+LAST MODIFIED DATE: 
+
+DESCRIPTION:
+    OUTPUT LAYER FOR EXT DASHBOARD
+PARAMETERS:
+    SOURCE_OBJECT       - user__transactions__daily_user_level
+*/
+
+WITH lc AS (
+    SELECT *, 'LOYALTY CARD' AS category
+    FROM {{ ref('user__transactions__daily_user_level') }}
+    WHERE channel IN ('LLOYDS', 'MIXR')),
+
+     final AS (
+    SELECT date, channel, brand, loyalty_plan_company, u007__active_users__user_level_daily__uid, category
+    FROM lc)
+
+SELECT *
+FROM final
