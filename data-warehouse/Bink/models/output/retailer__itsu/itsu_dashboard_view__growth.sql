dcb8b227e0364ef9500780df5d6e09360fb525b3
Chris Mitchell <cmitchell@bink.com>
2024-01-03 12:58:29 +0000

    code docs

--- data-warehouse/Bink/models/output/retailer__itsu/itsu_dashboard_view__growth.sql
+++ data-warehouse/Bink/models/output/retailer__itsu/itsu_dashboard_view__growth.sql
@@ -1,8 +1,8 @@
 /*
 Created by:         Christopher Mitchell
 Created date:       2023-07-05
-Last modified by:
-Last modified date:
+Last modified by:   Christopher Mitchell
+Last modified date: 2024-01-03
 
 Description:
     Datasource to produce tableau dashboard for itsu
