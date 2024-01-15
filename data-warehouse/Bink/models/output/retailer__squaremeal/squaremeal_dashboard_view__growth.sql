f4b2d1dbebcd7e61c9c7169317e7a529b222f92f
Chris Mitchell <cmitchell@bink.com>
2024-01-15 13:20:59 +0000

    boilerplate

---
+++ data-warehouse/Bink/models/output/retailer__squaremeal/squaremeal_dashboard_view__growth.sql
@@ -0,0 +1,174 @@
+/*
+Created by:         Christopher Mitchell
+Created date:       2024-01-15
+Last modified by:
+Last modified date:
+
+Description:
+    Datasource to produce tableau dashboard for SquareMeal
+Parameters:
+    source_object       - lc__links_joins__monthly_retailer
+                        - trans__trans__monthly_retailer
+                        - trans__avg__monthly_retailer
+                        - user__transactions__monthly_retailer
+*/
+
+with
+lc_metric as (
+    select
+        *,
+        'JOINS' as category
+    from {{ ref("lc__links_joins__monthly_retailer__growth") }}
+    where loyalty_plan_company = 'SquareMeal'
+),
+
+txn_metrics as (
+    select
+        *,
+        'SPEND' as category
+    from {{ ref("trans__trans__monthly_retailer__growth") }}
+    where loyalty_plan_company = 'SquareMeal'
+),
+
+txn_avg as (
+    select
+        *,
+        'SPEND' as category
+    from {{ ref("trans__avg__monthly_retailer__growth") }}
+    where loyalty_plan_company = 'SquareMeal'
+),
+
+user_metrics as (
+    select
+        *,
+        'USERS' as category
+    from {{ ref("user__transactions__monthly_retailer__growth") }}
+    where loyalty_plan_company = 'SquareMeal'
+),
+
+pll_metrics as (
+    select
+        *,
+        'JOINS' as category
+    from {{ ref("lc__pll__monthly_retailer__growth") }}
+    where loyalty_plan_company = 'SquareMeal'
+),
+
+combine_all as (
+    select
+        date,
+        category,
+        loyalty_plan_name,
+        loyalty_plan_company,
+        lc347__successful_loyalty_card_joins__monthly_retailer__count__growth,
+        lc351__successful_loyalty_card_links__monthly_retailer__dcount_user__growth,
+        lc334__successful_loyalty_card_join_mrkt_opt_in_per_successful_loyalty_card_join__monthly_retailer__percentage__growth,
+        null as lc201__loyalty_card_active_pll__monthly_retailer__pit__growth,
+        null as u107_active_users__retailer_monthly__dcount_uid__growth,
+        null as u108_active_users_retailer_monthly__cdcount_uid__growth,
+        null as t011__txns__monthly_retailer__dcount__growth,
+        null as t012__refund__monthly_retailer__dcount__growth,
+        null as t009__spend__monthly_retailer__sum__growth,
+        null as t014__aov__monthly_retailer__avg__growth,
+        null as t015__arpu__monthly_retailer__avg__growth,
+        null as t016__atf__monthly_retailer__avg__growth,
+        null as t026__txns_and_dupes__monthly_retailer__dcount__growth,
+        null as t019__duplicate_txn_per_txn__monthly_retailer__percentage__growth,
+        null as t017__duplicate_txn__monthly_retailer__dcount__growth
+    from lc_metric
+    union all
+    select
+        date,
+        category,
+        loyalty_plan_name,
+        loyalty_plan_company,
+        null as lc347__successful_loyalty_card_joins__monthly_retailer__count__growth__growth,
+        null as lc351__successful_loyalty_card_links__monthly_retailer__dcount_user__growth,
+        null
+            as lc334__successful_loyalty_card_join_mrkt_opt_in_per_successful_loyalty_card_join__monthly_retailer__percentage__growth,
+        null as lc201__loyalty_card_active_pll__monthly_retailer__pit__growth,
+        null as u107_active_users__retailer_monthly__dcount_uid__growth,
+        null as u108_active_users_retailer_monthly__cdcount_uid__growth,
+        t011__txns__monthly_retailer__dcount__growth,
+        t012__refund__monthly_retailer__dcount__growth,
+        t009__spend__monthly_retailer__sum__growth,
+        null as t014__aov__monthly_retailer__avg__growth,
+        null as t015__arpu__monthly_retailer__avg__growth,
+        null as t016__atf__monthly_retailer__avg__growth,
+        t026__txns_and_dupes__monthly_retailer__dcount__growth,
+        t019__duplicate_txn_per_txn__monthly_retailer__percentage__growth,
+        t017__duplicate_txn__monthly_retailer__dcount__growth
+    from txn_metrics
+    union all
+    select
+        date,
+        category,
+        loyalty_plan_name,
+        loyalty_plan_company,
+        null as lc347__successful_loyalty_card_joins__monthly_retailer__count__growth__growth,
+        null as lc351__successful_loyalty_card_links__monthly_retailer__dcount_user__growth,
+        null
+            as lc334__successful_loyalty_card_join_mrkt_opt_in_per_successful_loyalty_card_join__monthly_retailer__percentage__growth,
+        null as lc201__loyalty_card_active_pll__monthly_retailer__pit__growth,
+        null as u107_active_users__retailer_monthly__dcount_uid__growth,
+        null as u108_active_users_retailer_monthly__cdcount_uid__growth,
+        null as t011__txns__monthly_retailer__dcount__growth,
+        null as t012__refund__monthly_retailer__dcount__growth,
+        null as t009__spend__monthly_retailer__sum__growth,
+        t014__aov__monthly_retailer__avg__growth,
+        t015__arpu__monthly_retailer__avg__growth,
+        t016__atf__monthly_retailer__avg__growth,
+        null as t026__txns_and_dupes__monthly_retailer__dcount__growth,
+        null as t019__duplicate_txn_per_txn__monthly_retailer__percentage__growth,
+        null as t017__duplicate_txn__monthly_retailer__dcount__growth
+    from txn_avg
+    union all
+    select
+        date,
+        category,
+        loyalty_plan_name,
+        loyalty_plan_company,
+        null as lc347__successful_loyalty_card_joins__monthly_retailer__count__growth__growth,
+        null as lc351__successful_loyalty_card_links__monthly_retailer__dcount_user__growth,
+        null
+            as lc334__successful_loyalty_card_join_mrkt_opt_in_per_successful_loyalty_card_join__monthly_retailer__percentage__growth,
+        null as lc201__loyalty_card_active_pll__monthly_retailer__pit__growth,
+        u107_active_users__retailer_monthly__dcount_uid__growth,
+        u108_active_users_retailer_monthly__cdcount_uid__growth,
+        null as t011__txns__monthly_retailer__dcount__growth,
+        null as t012__refund__monthly_retailer__dcount__growth,
+        null as t009__spend__monthly_retailer__sum__growth,
+        null as t014__aov__monthly_retailer__avg__growth,
+        null as t015__arpu__monthly_retailer__avg__growth,
+        null as t016__atf__monthly_retailer__avg__growth,
+        null as t026__txns_and_dupes__monthly_retailer__dcount__growth,
+        null as t019__duplicate_txn_per_txn__monthly_retailer__percentage__growth,
+        null as t017__duplicate_txn__monthly_retailer__dcount__growth
+    from user_metrics
+    union all
+    select
+        date,
+        category,
+        loyalty_plan_name,
+        loyalty_plan_company,
+        null as lc347__successful_loyalty_card_joins__monthly_retailer__count__growth__growth,
+        null as lc351__successful_loyalty_card_links__monthly_retailer__dcount_user__growth,
+        null
+            as lc334__successful_loyalty_card_join_mrkt_opt_in_per_successful_loyalty_card_join__monthly_retailer__percentage__growth,
+        lc201__loyalty_card_active_pll__monthly_retailer__pit__growth,
+        null as u107_active_users__retailer_monthly__dcount_uid__growth,
+        null as u108_active_users_retailer_monthly__cdcount_uid__growth,
+        null as t011__txns__monthly_retailer__dcount__growth,
+        null as t012__refund__monthly_retailer__dcount__growth,
+        null as t009__spend__monthly_retailer__sum__growth,
+        null as t014__aov__monthly_retailer__avg__growth,
+        null as t015__arpu__monthly_retailer__avg__growth,
+        null as t016__atf__monthly_retailer__avg__growth,
+        null as t026__txns_and_dupes__monthly_retailer__dcount__growth,
+        null as t019__duplicate_txn_per_txn__monthly_retailer__percentage__growth,
+        null as t017__duplicate_txn__monthly_retailer__dcount__growth
+    from pll_metrics
+)
+
+select *
+from combine_all
---
+++ data-warehouse/Bink/models/output/retailer__squaremeal/squaremeal_dashboard_view_graphs.sql
@@ -0,0 +1,58 @@
+/*
+Created by:         Christopher Mitchell
+Created date:       2024-01-15
+Last modified by:   
+Last modified date: 
+
+Description:
+    Datasource to produce tableau dashboard for SquareMeal graphs
+Parameters:
+    source_object       - lc__links_joins__monthly_retailer
+                        - trans__trans__monthly_retailer
+                        - trans__avg__monthly_retailer
+                        - user__transactions__monthly_retailer
+*/
+
+with
+lc_metric as (
+    select
+        *,
+        'JOINS' as category
+    from {{ ref("lc__links_joins__daily_retailer") }}
+    where loyalty_plan_company = 'SquareMeal'
+),
+
+txn_metrics as (
+    select
+        *,
+        'SPEND' as category
+    from {{ ref("trans__trans__daily_retailer") }}
+    where loyalty_plan_company = 'SquareMeal'
+),
+
+combine_all as (
+    select
+        date,
+        category,
+        loyalty_plan_name,
+        loyalty_plan_company,
+        lc067__successful_loyalty_card_joins__daily_retailer__csum,
+        lc047__successful_loyalty_card_joins__daily_retailer__count,
+        null as t027__spend__daily_retailer__csum,
+        null as t033__spend__daily_retailer__sum
+    from lc_metric
+    union all
+    select
+        date,
+        category,
+        loyalty_plan_name,
+        loyalty_plan_company,
+        null as lc067__successful_loyalty_card_joins__daily_retailer__csum,
+        null as lc047__successful_loyalty_card_joins__daily_retailer__count,
+        t027__spend__daily_retailer__csum,
+        t033__spend__daily_retailer__sum
+    from txn_metrics
+)
+
+select *
+from combine_all
