8568c2fc89155260f614099afb27a85c5d6f0304
Chris Mitchell <cmitchell@bink.com>
2024-02-08 09:38:53 +0000

    Revert "service incremental"
    This reverts commit 48ae636d01ac22ca082215fd653d4b6b367d7d52.

--- data-warehouse/Bink/models/prod/service_data/service_management.sql
+++ data-warehouse/Bink/models/prod/service_data/service_management.sql
@@ -1,8 +1,8 @@
 /*
 Created by:         Anand Bhakta
 Created date:       2023-02-02
-Last modified by:   Christopher Mitchell
-Last modified date: 202402-05
+Last modified by:
+Last modified date:
 
 Description:
     Stages the freshservice table
@@ -10,19 +10,8 @@ Description:
 Parameters:
     source_object      - SERVICE_DATA.FRESHSERVICE
 */
-
-{{ config(materialized="incremental", unique_key="ID") }}
-
 with
-all_data as (
-    select *
-    from {{ ref("stg_service_data__FRESHSERVICE") }}
-    {% if is_incremental() %}
-        where
-            _airbyte_emitted_at
-            >= (select max(inserted_date_time) from {{ this }})
-    {% endif %}
-),
+all_data as (select * from {{ ref("stg_service_data__FRESHSERVICE") }}),
 
 add_most_recent as (
     select
