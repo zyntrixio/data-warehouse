1eb57025fa9de44c289fe9729a5457a1a72e3c97
Anand Bhakta <abhakta@bink.com>
2024-02-14 14:02:07 +0000

    adding incremental to pll transform table

--- data-warehouse/Bink/models/staging/transformation/transformed_pll.sql
+++ data-warehouse/Bink/models/staging/transformation/transformed_pll.sql
@@ -17,32 +17,56 @@ pll_events as (
     select *
     from {{ ref("stg_hermes__EVENTS") }}
     where event_type = 'pll_link.statuschange'
+    {% if is_incremental() %}
+        and
+            _airbyte_emitted_at
+            >= (select max(inserted_date_time) from {{ this }})
+    {% endif %}
 )
 
 ,select_fields as (
     select
+        event_id,
         event_date_time,
         json:internal_user_ref as user_id,
         json:scheme_account_id as loyalty_card_id,
         json:payment_account_id as payment_account_id,
-        json:to_state as to_status
+        null as to_date,
+        json:to_state = 1 as active_link,
+        null as inserted_date_time
     from pll_events
+)
+
+,union_old_pll_records as (
+    select *
+    from select_fields
+    {% if is_incremental() %}
+        union
+        select *
+        from {{ this }}
+        where
+            (loyalty_card_id, payment_account_id, user_id) in (
+                select loyalty_card_id, payment_account_id, user_id from select_fields
+            )
+    {% endif %}
 )
 
 ,from_to_dates as (
     select
+        event_id,
+        event_date_time as from_date,
         user_id,
         loyalty_card_id,
         payment_account_id,
-        event_date_time as from_date,
         coalesce(
             lead(event_date_time, 1) over (
             partition by loyalty_card_id, payment_account_id, user_id
             order by event_date_time asc
         ),
         current_timestamp) as to_date,
-        to_status = 1 as active_link
-    from select_fields
+        active_link,
+        sysdate() as inserted_date_time
+    from union_old_pll_records
     qualify active_link
 )
 
